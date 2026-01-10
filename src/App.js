0a07cae2ecee358a399d1019d883bb39dee781bf
Jenit Lal Shakya <jenit9408@gmail.com>
2026-01-10 10:35:20 +0545

    Improved Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Form from './components/Form';
-import { useState } from 'react';
+import React, { useState } from 'react';
 // import About from './components/About';
 
 function App() {
